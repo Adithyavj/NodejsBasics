f4a27ee88e005321675898bf78dc0d6fb1736dbc
Adithya Vijay K <adithyavj@gmailcom>
2021-03-08 20:52:41 +0530

    arrow function in es6

--- es6_arrowfun.js
+++ es6_arrowfun.js
@@ -1,3 +1,11 @@
 function add(a, b) {
   return a + b;
 }
+
+console.log(add(1, 2));
+
+//Arrow Function
+            //syntax of funtion, what function should do
+let add1 = (a, b) => a + b;
+
+console.log(add1(5,5))
