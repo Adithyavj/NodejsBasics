d43b79e9056c8284e5586fb85b6699f690796d1e
Adithya Vijay K <adithyavj@gmailcom>
2021-03-08 19:12:59 +0530

    object in Javascript

--- object_sample.js
+++ object_sample.js
@@ -1,2 +1,18 @@
 //Object is key value representation
-var Person={name:}
+var Person = { name: "Adithya Vijay K", age: 24, place: "Malappuram",display: function (){
+    console.log(this.name)
+}}
+console.log(Person.name)
+console.log(Person['age'])
+console.log('-----------------------')
+
+for(x in Person){
+    console.log(x)
+}
+
+console.log('-----------------------')
+for(x in Person){
+    console.log(Person[x])
+}
+console.log('-----------------------')
+console.log(Person.display())
