f42e269af8b601e98f32f455a47c01e0a8ec5c85
Adithya Vijay K <adithyavj@gmailcom>
2021-03-10 21:28:31 +0530

    asynchronous task example

--- callback_sample.js
+++ callback_sample.js
@@ -13,22 +13,36 @@
 // console.log(diff)
 
 //Synchronous concept....
-console.log('started')
-longTask(2000)
-console.log('end')
+// console.log('started')
+// longTask(2000)
+// console.log('end')
 
-console.log('started')
-longTask(2000)
-console.log('end')
+// console.log('started')
+// longTask(2000)
+// console.log('end')
 
-console.log('started')
-longTask(2000)
-console.log('end')
+// console.log('started')
+// longTask(2000)
 
+//Asynchronous
+console.log('Started')
+setTimeout(showEnd,2000) //setTimeout is an asynchronous function
+                         //this is an example of call back
+                         //once it is done it calls back the function
+
+console.log('Started')
+setTimeout(showEnd,2000)
+
+console.log('Started')
+setTimeout(showEnd,2000)
 
 function longTask(milliSecondTime){
     var dt=new Date() //current date
     while((new Date()-dt)<= milliSecondTime){
 
     }
+}
+
+function showEnd(){
+    console.log('end')
 }
