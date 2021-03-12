64822c7c8e63f23bb5715a52a9e01b3fc525439f
Adithya Vijay K <adithyavj@gmailcom>
2021-03-12 21:59:04 +0530

    creating response for different routes

--- Server Creation/sampleServerhtml.js
+++ Server Creation/sampleServerhtml.js
@@ -1,11 +1,20 @@
 var http = require("http");
 var fs = require("fs");
 
-http.createServer(function(req,res){
-    fs.readFile('sample.html',function(err,data){
-        res.writeHead(200,{'Content-Type':'text/html'})
-        res.write(data)
-        res.end
-    })
-
-}).listen(7000)
+http
+  .createServer(function (req, res) {
+    if (req.url === "/") {
+      fs.readFile("sample.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    } else if (req.url === "/login") {
+      res.write("Login-page");
+      res.end();
+    } else {
+        res.write('Error')
+        res.end()
+    }
+  })
+  .listen(7000,()=>console.log('Server Started...'));
