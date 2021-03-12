48c04e48a1efaa14f468030fb75b3067973372af
Adithya Vijay K <adithyavj@gmailcom>
2021-03-12 21:10:55 +0530

    createServer code changed

--- Server Creation/sampleServer.js
+++ Server Creation/sampleServer.js
@@ -1,10 +1,20 @@
 var http = require("http"); //module in node core
 
-http.createServer(server).listen(7000) //create server call back function
-
-//req - from where request is coming
-//res - how to respond
-function server(req,res){
+http.createServer(function(req,res){
     res.write('Test Server Created') 
     res.end()
-}
+}).listen(7000)
+
+
+//Create Server! - Code by writing callback function as seperate... refer below
+
+// http.createServer(server).listen(7000) //create server call back function
+
+// //req - from where request is coming
+// //res - how to respond
+// function server(req,res){
+//     res.write('Test Server Created') 
+//     res.end()
+// }
+
+
