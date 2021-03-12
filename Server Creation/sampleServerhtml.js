5de639af048f846c7aef771df43251810cdc7e64
Adithya Vijay K <adithyavj@gmailcom>
2021-03-12 22:43:25 +0530

    using url to parse result of response, taking request from diffrent routes

--- Server Creation/sampleServerhtml.js
+++ Server Creation/sampleServerhtml.js
@@ -1,20 +1,36 @@
 var http = require("http");
 var fs = require("fs");
+var url = require("url");
 
 http
   .createServer(function (req, res) {
-    if (req.url === "/") {
+    var q = url.parse(req.url, true);
+    //console.log(q.pathname)
+
+    if (q.pathname === "/") {
+      //req.url is used to check if it is host or route
       fs.readFile("sample.html", function (err, data) {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write(data);
         res.end();
       });
-    } else if (req.url === "/login") {
-      res.write("Login-page");
+    } else if (q.pathname === "/signup") {
+      fs.readFile("signup.html", (err, data) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    } else if (q.pathname === "/signupaction") {
+      //console.log(q.query); //calling query as JSON
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("<h1>First Name: " + q.query.fname + "</h1>");
+      res.write("<h1>Last Name: " + q.query.lname + "</h1>");
+      res.write("<h1>Mobile: " + q.query.mobile + "</h1>");
       res.end();
     } else {
-        res.write('Error')
-        res.end()
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.write("Error");
+      res.end();
     }
   })
-  .listen(7000,()=>console.log('Server Started...'));
+  .listen(7000, () => console.log("Server Started..."));
