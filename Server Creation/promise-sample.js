ffdd84d493eb2fc3394c58d8b6077ce0b2f4ceee
Adithya Vijay K <adithyavj@gmail.com>
2021-03-17 22:39:25 +0530

    Promise

--- Server Creation/promise-sample.js
+++ Server Creation/promise-sample.js
@@ -1,4 +1,3 @@
-
 //Call Back Hell
 function add(num1, num2, callback) {
   let err = false;
@@ -10,20 +9,57 @@ function add(num1, num2, callback) {
 function multiply(num1, num2, callback) {
   callback(num1 * num2);
 }
-function div(num1,num2,callback){
-    callback(num1/num2)
+function div(num1, num2, callback) {
+  callback(num1 / num2);
 }
 
-add(10, 20, (sum, err) => {
-  if (err) {
-    console.log("First No. is zero");
-  } else {
+const { reject, resolve } = require("promise");
+// add(10, 20, (sum, err) => {
+//   if (err) {
+//     console.log("First No. is zero");
+//   } else {
+//     console.log(sum);
+//     multiply(sum, sum, (product) => {
+//       console.log(product);
+//       div(product,10,(result)=>{
+//           console.log(result)
+//       })
+//     });
+//   }
+// });
+
+//Resolving Call Back Hell using Promise
+const Promise = require("promise");
+
+function add(num1, num2) {
+  return new Promise((resolve, reject) => {
+    if (num1 === 0) {
+      reject("Error");
+    }
+    resolve(num1 + num2);
+  });
+}
+function multiply(num1, num2) {
+  return new Promise((resolve, reject) => {
+    resolve(num1 * num2);
+  });
+}
+function div(num1, num2) {
+  return new Promise((resolve, reject) => {
+    resolve(num1 / num2);
+  });
+}
+
+
+//This is Called Chaining
+add(20, 10).then((sum) => {
     console.log(sum);
-    multiply(sum, sum, (product) => {
-      console.log(product);
-      div(product,10,(result)=>{
-          console.log(result)
-      })
-    });
-  }
-});
+    return multiply(sum, sum);
+  }).then((product) => {
+    console.log(product);
+    return div(product, 10);
+  }).then((result) => {
+    console.log(result);
+  }).catch((err) => {
+    console.log(err);
+  });
