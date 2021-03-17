876001e4dba929ce66ab57050624a5f3d2bd5912
Adithya Vijay K <adithyavj@gmail.com>
2021-03-17 22:53:34 +0530

    Promise All for side by side function execution

---
+++ Server Creation/promise-async-await.js
@@ -0,0 +1,23 @@
+const { resolve, reject } = require("promise");
+const Promise = require("promise");
+
+function getName() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hai");
+    }, 3000);
+  });
+}
+
+function getMobile() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("9946173471");
+    }, 2000);
+  });
+}
+
+//To call both functions together
+Promise.all([getName(), getMobile()]).then((result) => {
+  console.log(result);
+});
